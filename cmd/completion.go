47cd53666955faa7c8dec790951bd91ec3cef90f
Cristian Correa <cristian.correa.capetillo@gmail.com>
2026-06-23 20:27:58 -0400

    feat: autocompletado bash/zsh/fish (Tarea 12.3)

---
+++ cmd/completion.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish]",
+	Short: "Generar script de autocompletado para la shell",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletion(os.Stdout)
+		case "zsh":
+			return rootCmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			return rootCmd.GenFishCompletion(os.Stdout, true)
+		default:
+			return fmt.Errorf("shell no soportada: %s (usar: bash, zsh, fish)", args[0])
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
